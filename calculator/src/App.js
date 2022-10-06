7c8fae457e1d2402dd7332b7084bf12f4a9d5b1a
mulukimwangeka <mwangekaruth@gmail.com>
2022-10-06 11:50:55 +0300

    added the equalsClickHandler

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -60,6 +60,29 @@ const App = () => {
       num: 0,
     });
   };
+//equalsClickHandler in place 
+  const equalsClickHandler = () => {
+    if (calc.sign && calc.num) {
+      const math = (a, b, sign) =>
+        sign === "+"
+          ? a + b
+          : sign === "-"
+          ? a - b
+          : sign === "X"
+          ? a * b
+          : a / b;
+  
+      setCalc({
+        ...calc,
+        res:
+          calc.num === "0" && calc.sign === "/"
+            ? "Can't divide with 0"
+            : math(Number(calc.res), Number(calc.num), calc.sign),
+        sign: "",
+        num: 0,
+      });
+    }
+  };
 
   return (
     <Wrapper>
